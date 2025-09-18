81ea91c972f4ad8cf009b5fec372f00fce47fe5d
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-18 18:59:23 -0300

    Aula de Operadores Relacionais

---
+++ src/com/cursojava/basico/OperadoresRelacionais.java
@@ -0,0 +1,26 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        //Operadores relacionais retornam um valor boleano quando comparam duas expressoes
+
+        int valor1 = 20, valor2 = 10;
+
+        //Operador de igualdade (==)
+        System.out.println("O valor1 é igual ao valor2: "+ (valor1 == valor2));
+
+        //Operador menor que (<)
+        System.out.println("O valor1 é menor que o valor2: "+ (valor1 < valor2));
+
+        //Operador maior que (>)
+        System.out.println("O valor1 é maior que o valor2: "+ (valor1 > valor2));
+
+        //Operador menor ou igual a(<=)
+        System.out.println("O valor1 é menor ou igual ao valor2: "+ (valor1 <= valor2));
+
+        //Operador maior que (>=)
+        System.out.println("O valor1 é maior ou igual ao valor2: "+ (valor1 >= valor2));
+
+        //Operador diferente de (!=)
+        System.out.println("O valor1 é diferente de valor2: "+ (valor1 != valor2));
+
+    }
+}
