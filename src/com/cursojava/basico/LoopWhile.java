2dcd1f06a1ffa130856b53316a58307d50a303a6
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-19 11:10:16 -0300

    Aula de estrutura repetitiva while

---
+++ src/com/cursojava/basico/LoopWhile.java
@@ -0,0 +1,12 @@
+public class LoopWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        //while - Enquanto
+        while(contador <= 5){
+            IO.println(contador);
+            contador ++;
+        }
+        IO.println("Acabou a contagem...");
+    }
+}
