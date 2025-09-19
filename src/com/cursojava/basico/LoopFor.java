c812a0e1f749cb7b676f3e4685b2087fef8c2be1
edilsossilva-1976 <edilsossilva@gmail.com>
2025-09-19 11:15:03 -0300

    Aula de estrutura repetitiva for

---
+++ src/com/cursojava/basico/LoopFor.java
@@ -0,0 +1,10 @@
+public class LoopFor {
+    public static void main(String[] args) {
+
+        // for - para
+        for (int contador = 0; contador <= 10; contador++) {
+            IO.println("Linha " + contador);
+        }
+        IO.println("Acabou a contagem...");
+    }
+}
